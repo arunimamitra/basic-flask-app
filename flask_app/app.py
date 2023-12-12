28f4d70db038313e3d93e0d57f2817d4777c65f9
Anoushka Gupta <anoushka.post@gmail.com>
2023-12-12 15:45:35 -0500

    frontend v1

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,5 +1,5 @@
 # Importing essential libraries
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import torch
 import torch.nn as nn
 from torchvision import transforms, models
@@ -10,25 +10,34 @@ model2=models.resnet18()
 device='cpu'
 model2.fc = nn.Linear(model2.fc.in_features, 1)
 model2=model2.to(device)
-model2.load_state_dict(torch.load("model.pth", map_location=torch.device('cpu')))
+#model2.load_state_dict(torch.load("model.pth", map_location=torch.device('cpu')))
+
+# modelvgg=models.vgg19()
+# modelvgg.classifier[6]=nn.Linear(modelvgg.classifier[6].in_features, 1)
+# modelvgg=modelvgg.to(device)
+checkpoint = torch.load("best_model_27.pth", map_location=torch.device('cpu'))
+model_state_dict = checkpoint['model_state_dict']
+model2.load_state_dict(model_state_dict)
+# modelvgg.load_state_dict(model_state_dict,strict=False)
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home2.html')
+    return render_template('newhome.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
 
     if request.method == 'POST':
-        print("inside if condition")
-        img=Image.open('./real2.jpeg')
+        file = request.files['file']
+        img = Image.open(file)
         print(type(img))
         #output_string="Hello Arunima"
         output_string=predict_image(img)
         print(output_string)
-    return render_template('result2.html',prediction = output_string)
+    return output_string    
+    #return render_template('result2.html',prediction = output_string)
 
 def predict_image(img):
     transform_norm = transforms.Compose([
@@ -47,9 +56,19 @@ def predict_image(img):
         num = output.data[0]
         print((float)(num[0]))
         if(float)(num[0]) > 0.5:
-            return "fake"
+            return "Fake"
         else:
-            return "real"
+            return "Real"
+        # modelvgg.eval()  
+        # output =modelvgg(img_normalized)
+        # output=torch.sigmoid(output)
+        # print(output)
+        # num = output.data[0]
+        # print((float)(num[0]))
+        # if(float)(num[0] > 0.5):
+        #     return "Fake"
+        # else:
+        #     return "Real"
 
 
 if __name__ == '__main__':
